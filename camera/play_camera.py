3a41889da480f80eb91956e4801ee56147b15d84
s7ev3n <s.eleven.73@gmail.com>
2024-11-14 11:24:48 +0800

    add cylindarl camera

--- camera/play_camera.py
+++ camera/play_camera.py
@@ -30,6 +30,11 @@ fake_image_cuda = torch.from_numpy(fake_image.transpose(2,0,1)).to("cuda")
 fake_intrinsics = fake_intrinsics.to("cuda")
 fake_extrinsics = fake_extrinsics.to("cuda")
 
+camera_model_mappings = {
+    "Pinhole": PinholeCameraModel,
+    "Cylindrical": CylindricalCameraModel
+}
+
 def update_focal_scaling_image(old_intrinsics, new_focal_length):
     new_intrinsics = old_intrinsics.clone()
     new_intrinsics[0, 0] = new_focal_length
@@ -54,21 +59,30 @@ def update_focal_scaling_image(old_intrinsics, new_focal_length):
 def main() -> None:
     server = viser.ViserServer()
 
-    # Add GUI
-    with server.gui.add_folder("Adjust Intrinsics"):
-        gui_camera_choice = server.gui.add_dropdown(
-            "Camera Model",
-            ("Pinhole", "Cylindral")
+    # Camera model switch
+    default_camera_model = "Pinhole"
+    with server.gui.add_folder("Camera Model"):
+        gui_camera_model_display = server.gui.add_text(
+            "Camera Model: ",
+            default_camera_model,
+            disabled=True
         )
+        gui_camera_model_switch = server.gui.add_button_group(
+            "Camera Model:",
+            ["Pinhole", "Cylindrical"]
+        )
+
+    # Camera parameters adjustment
+    with server.gui.add_folder("Adjust Intrinsics"):  
         gui_focal_display = server.gui.add_number(
-            "Focal Length: ",
+            "Current Focal Length: ",
             initial_value=fx,
             disabled=True
         )
         gui_focal_slider = server.gui.add_slider(
                                 "Focal Length Scaling",
                                 min=1,
-                                max=1.5,
+                                max=1.8,
                                 step=0.1,
                                 initial_value=1
                             )
@@ -82,6 +96,26 @@ def main() -> None:
                                 wxyz=(1, 0, 0, 0),
                                 position=(0.0, 0.0, 1.0))
     
+    def update_camera_model_switch_image(from_camera, to_camera):
+        from_camera_model = camera_model_mappings[from_camera]
+        to_camera_model = camera_model_mappings[to_camera]
+        grid_uv, _, _ = get_grid_rectification(
+            fake_intrinsics,
+            fake_extrinsics,
+            from_camera_model,
+            fake_image_size_hw,
+            fake_intrinsics,
+            fake_extrinsics,
+            to_camera_model,
+            fake_image_size_hw,
+            do_normalization=False
+            )
+
+        image_rect = perform_rectification(fake_image_cuda.float(), grid_uv)
+        
+        return image_rect.cpu().numpy().astype(np.uint8).transpose(1, 2, 0)
+
+
     @gui_focal_slider.on_update
     def _(_) -> None:
         scale = gui_focal_slider.value
@@ -91,7 +125,14 @@ def main() -> None:
         new_image = update_focal_scaling_image(fake_intrinsics, new_focal_length)
         update_image(new_image)
         
-
+    @gui_camera_model_switch.on_click
+    def _(_):
+        if gui_camera_model_switch.value != gui_camera_model_display.value:
+            new_image = update_camera_model_switch_image(
+                gui_camera_model_display.value, 
+                gui_camera_model_switch.value)
+            gui_camera_model_display.value = gui_camera_model_switch.value
+            update_image(new_image)            
 
     with server.gui.add_folder("Adjust Extrinsics"):
         gui_camera_tran = server.gui.add_vector3(
